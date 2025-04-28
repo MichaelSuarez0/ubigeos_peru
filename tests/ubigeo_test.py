772b964f9d166793b4f57208b19e2b317eb9291c
MichaelSuarez0 <a20180264@pucp.edu.pe>
2025-04-28 00:11:47 -0500

    Update tests

--- tests/ubigeo_test.py
+++ tests/ubigeo_test.py
@@ -5,4 +5,12 @@ assert ubg.get_departamento(10101) == "AMAZONAS"
 
 assert ubg.normalize_departamento("HUANUCO", upper= False) == "Huánuco"
 
+assert ubg.get_macrorregion("AMAZONAS") == "Oriente"
+assert ubg.get_macrorregion("Amazonas") == "Oriente"
+assert ubg.get_macrorregion("01") == "Oriente"
+assert ubg.get_macrorregion(1) == "Oriente"
+
+assert ubg.get_ubigeo("Lince", "distritos") == "150116"
+assert ubg.get_ubigeo("Huaral", "provincia") == "1506"
+
 # pyinstrument -r html -o import_excel_automation.html -c "import ubigeos_peru"
