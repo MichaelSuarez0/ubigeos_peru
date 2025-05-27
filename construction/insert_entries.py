4176be5906a2c1788a9402768eec1562a50919e7
Michael Suárez <71056455+MichaelSuarez0@users.noreply.github.com>
2025-05-27 13:12:19 -0500

    Update insert_entries.py

--- construction/insert_entries.py
+++ construction/insert_entries.py
@@ -1,12 +1,50 @@
 import os
+from typing import Literal
 import orjson
-from collections import defaultdict
-from _functions import RESOURCES_PATH, write_to_resources, write_to_readable
+from _functions import update_to_readable, update_to_resources
 
 # TODO: Agregar:
 # Yauri (Espinar) (distritos)
 # TODO: Por agregar:
 equivalencias = {
+    'departamentos':{
+        'CUZCO': 'Cusco'
+    },
+    'provincias':{
+        'CUZCO': 'Cusco'
+    },
     'distritos':{
-        'COTABAMBA': 'Cotabambas'
+        'COTABAMBA': 'Cotabambas',
+        'SAN JERONIMO DE TUNAN': 'San Jerónimo de Tunán',
+        'SAN AGUSTIN' : 'San Agustín de Cajas',
+        'CUZCO': 'Cusco'
     }}
+
+distritos = {
+    'inei': {
+        '070107': 'Mi Perú'
+    },
+    'reniec' : {
+        '240107': 'Mi Perú'
+    },
+    'sunat': {
+        "120124": "Pariahuanca",
+        "080807": "Suyckutambo",
+        "080903": "Huayopata",
+        "080905": "Ocobamba"
+        }
+}
+
+def update_all(entries: dict, resource_name: Literal["departamentos", "distritos", "equivalencias", "inverted", "macrorregiones", "otros", "provincias"]):
+    resources_names = ["departamentos", "distritos", "provincias", "equivalencias", "inverted", "macrorregiones", "otros"]
+    if resource_name not in resources_names:
+        raise TypeError(f"Entries must have any of following names: {resources_names}")
+    update_to_resources(entries, resource_name)
+    update_to_readable(entries, resource_name)
+    if resource_name in resources_names[:3]:
+        from inverted_creation import inverted_creation
+        inverted_creation()
+
+
+if __name__ == "__main__":
+    update_all(distritos, "distritos")
