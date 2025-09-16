9a73eeeb0ac004760b6d050aeaa2119892162eec
Michael Suárez <71056455+MichaelSuarez0@users.noreply.github.com>
2025-09-15 23:59:34 -0500

    Functions support SeriesLike, tests missing

--- src/ubigeos_peru/core/departamento.py
+++ src/ubigeos_peru/core/departamento.py
@@ -1,5 +1,11 @@
 from typing import Literal
-from .utils import eliminar_acentos
+from ._utils import (
+    eliminar_acentos,
+    is_series_like,
+    SeriesLike,
+    reconstruct_like,
+    assert_error,
+)
 from .resource_manager import ResourceManager
 
 
@@ -14,82 +20,142 @@ class Departamento:
     @classmethod
     def validate_departamento(
         cls,
-        nombre_departamento: str,
+        nombre_departamento: str | SeriesLike,
         normalize: bool = False,
         on_error: Literal["raise", "ignore", "capitalize"] = "raise",
-    ) -> str:
-        
+    ) -> str | SeriesLike:
         cls._resources._load_resource_if_needed("equivalencias")
+        mapping = cls._resources._loaded["equivalencias"]["departamentos"]
 
-        # if cls["equivalencias"] is None:
-        #     raise RuntimeError("No se pudieron cargar las equivalencias")
-        if not isinstance(nombre_departamento, str):
-            try:
-                str(nombre_departamento)
-            except TypeError:
-                raise TypeError(
-                    f"No se permiten otros tipos de datos que no sean str, se insertó {type(nombre_departamento)}"
-                )
+        # ---------------------- Input: Series-like ----------------------
+        if is_series_like(nombre_departamento):
+            mapping: dict[str, str] = (
+                {k: eliminar_acentos(v).upper() for k, v in mapping.items()}
+                if normalize
+                else mapping
+            )
+            out = []
+            for item in nombre_departamento:
+                if not isinstance(item, str):
+                    try:
+                        str(item)
+                    except TypeError:
+                        raise TypeError(
+                            f"No se permiten otros tipos de datos que no sean str, se insertó {type(item)}"
+                        )
 
-        departamento = eliminar_acentos(nombre_departamento).strip().upper()
-        try:
-            resultado = cls._resources._loaded["equivalencias"]["departamentos"][departamento]
-        except KeyError:
-            if on_error == "raise":
-                raise KeyError(
-                    f"No se ha encontrado el departamento {nombre_departamento}"
-                )
-            elif on_error == "ignore":
-                resultado = nombre_departamento
-            elif on_error == "capitalize":
-                resultado = nombre_departamento.capitalize()
-            else:
-                raise ValueError(
-                    'El arg "on_error" debe ser uno de los siguientes: "raise", "ignore", "capitalize"'
-                )
-
-        if not normalize:
-            return resultado
-        else:
-            return eliminar_acentos(resultado).strip().upper()
-
-    @classmethod
-    def validate_ubicacion(
-        cls,
-        nombre_ubicacion: str,
-        normalize: bool = False,
-        on_error: Literal["raise", "ignore", "capitalize"] = "raise",
-    ) -> str:
-        
-        cls._resources._load_resource_if_needed("equivalencias")
-        nombre_ubicacion = eliminar_acentos(nombre_ubicacion).strip().upper()
-        try:
-            resultado = cls._resources._loaded["equivalencias"]["departamentos"][nombre_ubicacion]
-        except KeyError:
-            try:
-                resultado = cls._resources._loaded["equivalencias"]["provincias"][
-                    nombre_ubicacion
-                ]
-            except KeyError:
+                departamento = eliminar_acentos(item).strip().upper()
                 try:
-                    resultado = cls._resources._loaded["equivalencias"]["distritos"][
-                        nombre_ubicacion
-                    ]
+                    resultado = out.append(mapping[departamento])
                 except KeyError:
                     if on_error == "raise":
-                        raise KeyError(
-                            f"No se encontró el lugar {nombre_ubicacion} en la base de datos de departamentos, provincias o distritos"
-                        )
+                        raise KeyError(f"No se ha encontrado el departamento {item}")
                     elif on_error == "ignore":
-                        resultado = nombre_ubicacion
+                        resultado = item
                     elif on_error == "capitalize":
-                        resultado = nombre_ubicacion.capitalize()
+                        resultado = item.capitalize()
                     else:
                         raise ValueError(
                             'El arg "on_error" debe ser uno de los siguientes: "raise", "ignore", "capitalize"'
                         )
 
-        if not normalize:
-            return resultado
+            return reconstruct_like(nombre_departamento, out)
+
         else:
-            return eliminar_acentos(resultado).upper()
+            # ------------------------ Input: Singular ------------------------
+            if not isinstance(nombre_departamento, str):
+                try:
+                    str(nombre_departamento)
+                except TypeError:
+                    raise TypeError(
+                        f"No se permiten otros tipos de datos que no sean str, se insertó {type(nombre_departamento)}"
+                    )
+
+            departamento = eliminar_acentos(nombre_departamento).strip().upper()
+            try:
+                resultado = mapping[departamento]
+            except KeyError:
+                if on_error == "raise":
+                    raise KeyError(
+                        f"No se ha encontrado el departamento {nombre_departamento}"
+                    )
+                elif on_error == "ignore":
+                    resultado = nombre_departamento
+                elif on_error == "capitalize":
+                    resultado = nombre_departamento.capitalize()
+                else:
+                    raise ValueError(
+                        'El arg "on_error" debe ser uno de los siguientes: "raise", "ignore", "capitalize"'
+                    )
+
+            if not normalize:
+                return resultado
+            else:
+                return eliminar_acentos(resultado).strip().upper()
+
+    @classmethod
+    def validate_ubicacion(
+        cls,
+        nombre_ubicacion: str | SeriesLike,
+        normalize: bool = False,
+        on_error: Literal["raise", "ignore", "capitalize"] = "raise",
+    ) -> str | SeriesLike:
+        cls._resources._load_resource_if_needed("equivalencias")
+        mapping = cls._resources._loaded["equivalencias"]
+
+        if is_series_like(nombre_ubicacion):
+            mapping: dict[str, str] = (
+                {k: eliminar_acentos(v).upper() for k, v in mapping.items()}
+                if normalize
+                else mapping
+            )
+
+            out = []
+            for item in nombre_ubicacion:
+                item = eliminar_acentos(item).strip().upper()
+                try:
+                    resultado = mapping["departamentos"][nombre_ubicacion]
+                except KeyError:
+                    try:
+                        resultado = mapping["provincias"][nombre_ubicacion]
+                    except KeyError:
+                        try:
+                            resultado = mapping["distritos"][nombre_ubicacion]
+                        except KeyError:
+                            if on_error == "raise":
+                                raise KeyError(
+                                    f"No se encontró el lugar {nombre_ubicacion} en la base de datos de departamentos, provincias o distritos"
+                                )
+                            elif on_error == "ignore":
+                                resultado = nombre_ubicacion
+                            elif on_error == "capitalize":
+                                resultado = nombre_ubicacion.capitalize()
+                            else:
+                                raise ValueError(
+                                    'El arg "on_error" debe ser uno de los siguientes: "raise", "ignore", "capitalize"'
+                                )
+
+                out.append(resultado)
+            return reconstruct_like(nombre_ubicacion, out)
+
+        else:
+            nombre_ubicacion = eliminar_acentos(nombre_ubicacion).strip().upper()
+            try:
+                resultado = mapping["departamentos"][nombre_ubicacion]
+            except KeyError:
+                try:
+                    resultado = mapping["provincias"][nombre_ubicacion]
+                except KeyError:
+                    try:
+                        resultado = mapping["distritos"][nombre_ubicacion]
+                    except KeyError:
+                        resultado = assert_error(
+                            on_error,
+                            resultado,
+                            message="No se encontró el lugar {} en la base de datos de departamentos, provincias o distritos",
+                        )
+
+            if not normalize:
+                return resultado
+            else:
+                return eliminar_acentos(resultado).upper()
