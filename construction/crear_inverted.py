646baac742b9c2ea0ca8bc28ffc4c5f588bd81bb
Michael Suárez <71056455+MichaelSuarez0@users.noreply.github.com>
2025-10-20 22:50:48 -0500

    Updated inverted.json

--- construction/crear_inverted.py
+++ construction/crear_inverted.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 import orjson
-from _utils import RESOURCES_PATH, write_to_readable, write_to_resources
+from _utils import RESOURCES_PATH, write_to_readable, write_to_resources, eliminar_acentos
 
 
 def read_dict(level: str):
@@ -22,7 +22,7 @@ def join_dicts(dictionaries: list[dict], names: list[str]) -> dict:
 def invert_dict(final_dict: dict):
     final_dict = {
         level: {
-            inst: {lugar: code for code, lugar in mappings.items()}
+            inst: {eliminar_acentos(lugar.upper()): code for code, lugar in mappings.items()}
             for inst, mappings in institutions.items()
         }
         for level, institutions in final_dict.items()
